c974566f6eb8f189227077fdde0a6edee094d2ac
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-05-11 07:20:11 +0600

    all bookings section design and data loaded

--- src/Pages/BookingDetails/BookingDetails.jsx
+++ src/Pages/BookingDetails/BookingDetails.jsx
@@ -1,24 +1,89 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { AuthContext } from '../../Providers/AuthProviders';
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../Providers/AuthProviders";
 
 const BookingDetails = () => {
-    const {user} = useContext(AuthContext);
-    const [bookings, setBookings] = useState([]);
-    const url = `http://localhost:5000/all-bookings?email=${user.email}`;
-    useEffect(()=>{
-        fetch(url)
-                  .then(res=>res.json())
-                  .then(data=>{
-                        setBookings(data)
-                    })
-                  .catch(err=>console.log(err));
-    },[])
-    console.log(bookings)
-    return (
-        <div>
-            <h1 className='text-3xl font-bold font-mono'>Your Booking: <span className='text-secondary'>{bookings.length}</span></h1>
-        </div>
-    );
+  const { user } = useContext(AuthContext);
+  const [bookings, setBookings] = useState([]);
+  const url = `http://localhost:5000/all-bookings?email=${user.email}`;
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setBookings(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  console.log(bookings);
+  return (
+    <div className="my-10 md:my-20">
+      <h1 className="text-3xl font-bold font-mono">
+        Your Booking: <span className="text-secondary">{bookings.length}</span>
+      </h1>
+      <div className="overflow-x-auto w-full">
+        <table className="table w-full">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <th>Image</th>
+              <th>Price</th>
+              <th>Date</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {bookings.map((booking) => (
+              <>
+                <tr>
+                  <th>
+                    <label>
+                      <input type="checkbox" className="checkbox" />
+                    </label>
+                  </th>
+                  <td>
+                    <div className="flex items-center space-x-3">
+                      <div className="avatar">
+                        <div className="mask mask-squircle w-12 h-12">
+                          <img
+                            src={booking.img}
+                          />
+                        </div>
+                      </div>
+                      <div>
+                        <div className="font-bold">{booking.title}</div>
+                        <div className="text-sm opacity-50"></div>
+                      </div>
+                    </div>
+                  </td>
+                  <td>{booking.price}</td>
+                  <th>
+                    <div className="">{booking.date}</div>
+                  </th>
+                  <th>
+                    <div>Pending</div>
+                  </th>
+                </tr>
+              </>
+            ))}
+          </tbody>
+          {/* foot */}
+          <tfoot>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Job</th>
+              <th>Favorite Color</th>
+              <th></th>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default BookingDetails;
